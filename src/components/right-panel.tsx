634a8ca0d99a7d725517ef7b01aaa62f64356d76
pjanik <janikpiotrek@gmail.com>
2023-06-30 17:42:25 +0200

    feat: add time slider under graph [PT-185519783]

--- src/components/right-panel.tsx
+++ src/components/right-panel.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react";
-import React, { useState } from "react";
-import { Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Button, Slider } from "@mui/material";
 import { clsx } from "clsx";
 import { VegetationGraph } from "./vegetation-graph";
 import { useStores } from "../use-stores";
@@ -8,10 +8,12 @@ import { log } from "@concord-consortium/lara-interactive-api";
 
 import css from "./right-panel.scss";
 
+const RECENT_DATA_RANGE = 51;
 
 export const RightPanel = observer(() => {
   const { ui, simulation } = useStores();
   const [allData, setAllData] = useState(false);
+  const [graphEndPoint, setGraphEndPoint] = useState(0);
 
   const handleToggleDrawer = (e: React.SyntheticEvent) => {
     ui.toggleChart();
@@ -33,17 +35,50 @@ export const RightPanel = observer(() => {
     setAllData(!allData);
   };
 
+  const handleSliderChange = (e: Event, value: number | number[]) => {
+    setGraphEndPoint(value as number);
+  };
+
+  useEffect(() => {
+    // Reset graph offset when simulation is running and new data point is added.
+    setGraphEndPoint(simulation.yearlyVegetationStatistics.length);
+  }, [simulation.yearlyVegetationStatistics.length]);
+
+  const showRangeSlider = !allData && simulation.yearlyVegetationStatistics.length > RECENT_DATA_RANGE;
+
   return (
-    <div className={clsx(css.rightPanel, {[css.open]: ui.showChart, [css.wide]: simulation.config.graphWideAllData && allData })}>
+    <div className={clsx(css.rightPanel, { [css.open]: ui.showChart, [css.wide]: simulation.config.graphWideAllData && allData })}>
       <div className={css.rightPanelContent}>
-        <VegetationGraph allData={allData} />
+        <VegetationGraph allData={allData} recentDataEndPoint={graphEndPoint} recentDataLength={RECENT_DATA_RANGE} />
         <div className={css.graphControls}>
-          <Button className={css.allDataBtn} onClick={handleShowAllData}>{allData ? "Show Recent Data": "Show All Data"}</Button>
+          <div className={css.sliderContainer}>
+            {
+              true &&
+              <div className={css.slider}>
+                <Slider
+                  classes={{
+                    track: css.track,
+                    rail: css.rail,
+                    thumb: css.sliderThumb,
+                  }}
+                  size="small"
+                  step={1}
+                  min={0}
+                  max={70}
+                  // min={RECENT_DATA_RANGE}
+                  // max={simulation.yearlyVegetationStatistics.length}
+                  value={graphEndPoint}
+                  onChange={handleSliderChange}
+                />
+              </div>
+            }
+          </div>
+          <Button className={css.allDataBtn} onClick={handleShowAllData}>{allData ? "Show Recent Data" : "Show All Data"}</Button>
         </div>
       </div>
       <div className={css.rightPanelTabs}>
         <div className={css.rightPanelTab} onClick={handleToggleDrawer}>
-            <div className={css.tabContent}>Graphs</div>
+          <div className={css.tabContent}>Graphs</div>
         </div>
       </div>
     </div>
