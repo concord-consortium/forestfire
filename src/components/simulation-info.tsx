a1e50445ea705d903d1c153e23c7d8980800e9c7
pjanik <janikpiotrek@gmail.com>
2024-02-19 22:32:53 +0900

    feat: add thermometer widget [PT-187047154]

--- src/components/simulation-info.tsx
+++ src/components/simulation-info.tsx
@@ -2,19 +2,15 @@ import React from "react";
 import { observer } from "mobx-react";
 import { useStores } from "../use-stores";
 import { WindDial, degToCompass } from "./wind-dial";
-import { DroughtLevel, Vegetation } from "../types";
+import { Vegetation } from "../types";
 import { clsx } from "clsx";
 
 import css from "./simulation-info.scss";
-
-const avgTempLabels = ["Low", "Normal", "High", "Very High", "Extreme"];
+import { Thermometer } from "./thermometer";
 
 export const SimulationInfo = observer(function WrappedComponent() {
   const { simulation } = useStores();
-  const showTempInfo = simulation.initialDroughtLevel !== simulation.finalDroughtLevel;
   const scaledWind = simulation.wind.speed / simulation.config.windScaleFactor;
-  const avgTemp = (simulation.droughtLevel / DroughtLevel.SevereDrought) * avgTempLabels.length;
-  const avgTempLabel = avgTempLabels[Math.min(avgTempLabels.length - 1, Math.floor(avgTemp))];
   const stats = simulation.vegetationStatistics;
 
   const getStat = (vegetation: Vegetation | "burned") => `${Math.round(stats[vegetation] * 100)}%`;
@@ -32,15 +28,10 @@ export const SimulationInfo = observer(function WrappedComponent() {
         <div className={css.stat}><div className={clsx(css.box, css.burned)}>{ getStat("burned") }</div> Burned</div>
       </div>
 
-      {
-        showTempInfo &&
-        <div className={css.container} >
-          <div className={css.header}>Average Temp.</div>
-          <div className={css.text}>
-            {`${avgTemp.toFixed(2)}: ${avgTempLabel}`}
-          </div>
-        </div>
-      }
+      <div className={css.container} >
+        <div className={css.header}>Average Temp.</div>
+        <Thermometer droughtLevel={simulation.droughtLevel} />
+      </div>
 
       <div className={clsx(css.container, css.wind, { [css.windDidChange] : simulation.windDidChange })} >
         <div className={css.header}>Wind</div>
