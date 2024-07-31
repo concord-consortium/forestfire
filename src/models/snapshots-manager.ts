2865af4a2c68044e0a263b303f82c5d3ce88cc97
pjanik <janikpiotrek@gmail.com>
2024-07-31 15:07:44 +0200

    fix: fix scenario when user starts simulation with fire event [PT-187651915]

--- src/models/snapshots-manager.ts
+++ src/models/snapshots-manager.ts
@@ -30,7 +30,7 @@ export class SnapshotsManager {
     this.reset();
   }
 
-  get lastSnapshot() {
+  get lastSnapshot(): ISnapshot | null {
     return this.snapshots[this.snapshots.length - 1] ?? null;
   }
 
@@ -46,36 +46,69 @@ export class SnapshotsManager {
     return this.lastSnapshotTime !== null && this.simulation.time < this.lastSnapshotTime;
   }
 
-  saveSnapshot(existingCellSnapshots?: ISimulationSnapshot["cellSnapshots"]) {
+  public tooCloseToLastSnapshot(year: number) {
+    return this.lastSnapshotYear !== null && Math.abs(year - this.lastSnapshotYear) < MIN_DISTANCE_TO_FIRE_EVENT;
+  }
+
+  public isFireEventSnapshot(snapshot: ISnapshot | null): boolean {
+    return !!snapshot && snapshot.simulationSnapshot.sparks.length > 0;
+  }
+
+  public findClosestSnapshot(year: number): ISnapshot | null {
+    let minDiff = Infinity;
+    let closestSnapshot: ISnapshot | null = null;
+    for (const snapshot of this.snapshots) {
+      const snapshotYear = snapshot.simulationSnapshot.time / yearInMinutes;
+      const diff = Math.abs(year - snapshotYear);
+      if (diff <= minDiff) {
+        minDiff = diff;
+        closestSnapshot = snapshot;
+      }
+    }
+    return closestSnapshot;
+  }
+
+  public saveSnapshot(existingCellSnapshots?: ISimulationSnapshot["cellSnapshots"]) {
     const snapshot = this.simulation.snapshot(existingCellSnapshots);
     this.snapshots.push({ simulationSnapshot: snapshot });
   }
 
-  tooCloseToLastSnapshot(year: number) {
-    return this.lastSnapshotYear !== null && Math.abs(year - this.lastSnapshotYear) < MIN_DISTANCE_TO_FIRE_EVENT;
+  public restoreSnapshot(snapshot: ISnapshot) {
+    this.simulation.restoreSnapshot(snapshot.simulationSnapshot);
+    this.simulation.updateCellsStateFlag();
   }
 
-  isFireEventSnapshot(snapshot: ISnapshot) {
-    return snapshot.simulationSnapshot.sparks.length > 0;
+  @action.bound public reset() {
+    this.snapshots = [];
+    this.maxYear = 0;
   }
 
-  @action.bound public onFireEventEnded() {
-    if (!this.isFireEventSnapshot(this.lastSnapshot) && this.tooCloseToLastSnapshot(this.simulation.timeInYears)) {
-      // Remove previous regular snapshot if it's too close to the fire event snapshot. This will help users to
-      // snap to the fire event snapshot when scrubbing the timeline.
-      this.snapshots.pop();
-    }
-    this.saveSnapshot();
-  }
+  // Simulation event handlers:
 
   @action.bound public onStart() {
     this.maxYear = 0;
+    // Skip snapshot handling when a fire event is active. A snapshot will be taken at the end of the fire event.
+    if (this.simulation.isFireEventActive) {
+      return;
+    }
     if (this.snapshots.length === 0) {
       this.saveSnapshot();
     }
   }
 
+  @action.bound public onStop() {
+    // Skip snapshot handling when a fire event is active. A snapshot will be taken at the end of the fire event.
+    if (this.simulation.isFireEventActive) {
+      return;
+    }
+    this.saveSnapshot();
+  }
+
   @action.bound public onResume() {
+    // Skip snapshot handling when a fire event is active. A snapshot will be taken at the end of the fire event.
+    if (this.simulation.isFireEventActive) {
+      return;
+    }
     if (this.lastSnapshot && this.lastSnapshot.simulationSnapshot.time !== this.simulation.time) {
       this.restoreSnapshot(this.lastSnapshot);
     }
@@ -83,11 +116,11 @@ export class SnapshotsManager {
     this.snapshots.pop();
   }
 
-  @action.bound public onStop() {
-    this.saveSnapshot();
-  }
-
   @action.bound public onYearChange() {
+    // Skip snapshot handling when a fire event is active. A snapshot will be taken at the end of the fire event.
+    if (this.simulation.isFireEventActive) {
+      return;
+    }
     if (this.simulation.timeInYears > this.maxYear) {
       this.maxYear = this.simulation.timeInYears;
     }
@@ -111,27 +144,12 @@ export class SnapshotsManager {
     }
   }
 
-  public findClosestSnapshot(year: number): ISnapshot | null {
-    let minDiff = Infinity;
-    let closestSnapshot: ISnapshot | null = null;
-    for (const snapshot of this.snapshots) {
-      const snapshotYear = snapshot.simulationSnapshot.time / yearInMinutes;
-      const diff = Math.abs(year - snapshotYear);
-      if (diff <= minDiff) {
-        minDiff = diff;
-        closestSnapshot = snapshot;
-      }
+  @action.bound public onFireEventEnded() {
+    if (!this.isFireEventSnapshot(this.lastSnapshot) && this.tooCloseToLastSnapshot(this.simulation.timeInYears)) {
+      // Remove previous regular snapshot if it's too close to the fire event snapshot. This will help users to
+      // snap to the fire event snapshot when scrubbing the timeline.
+      this.snapshots.pop();
     }
-    return closestSnapshot;
-  }
-
-  public restoreSnapshot(snapshot: ISnapshot) {
-    this.simulation.restoreSnapshot(snapshot.simulationSnapshot);
-    this.simulation.updateCellsStateFlag();
-  }
-
-  @action.bound public reset() {
-    this.snapshots = [];
-    this.maxYear = 0;
+    this.saveSnapshot();
   }
 }
